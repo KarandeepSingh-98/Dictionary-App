b8e633611c2e7fb08821ff647a7a806a4bebe75a
KarandeeSingh-98 <singhkaran98998@gmail.com>
2025-07-23 11:48:17 +0530

    Changes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const getData = async (inputValue) => {
             <p>${jsonData[0].meanings[0].partOfSpeech}</p>
             <p>Meaning: <span>${jsonData[0].meanings[0].definitions[0].definition}</span></p>
             <p>Example: <span>${jsonData[0].meanings[0].definitions[0].example == undefined ? "Not Found" : jsonData[0].meanings[0].definitions[0].example}</span></p>
-            <p>Synonyms: <span>${jsonData[0].meanings[0].synonyms[0]}</span></p>
+            <p>Synonyms: <span>${jsonData[0].meanings[0].synonyms[0] == undefined ? "Not Found" : jsonData[0].meanings[0].synonyms[0]}</span></p>
             <a href=${jsonData[0].sourceUrls[0]} target="_blank"> Read more</a>
     `
 
